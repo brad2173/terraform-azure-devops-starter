22917c4b4712c91ab83b3906208e2424536905be
LeBlanc, Brad <brad.leblanc@vumc.org>
2023-06-28 08:07:23 -0500

    Updated Variables for Instance

--- 101-terraform-job/terraform/variables.tf
+++ 101-terraform-job/terraform/variables.tf
@@ -13,11 +13,11 @@ variable "environment" {
 variable "location" {
   type    = string
   description = "Azure region where to create resources."
-  default = "North Europe"
+  default = "East US"
 }
 
 variable "department" {
   type    = string
   description = "A sample variable passed from the build pipeline and used to tag resources."
-  default = "Engineering"
+  default = "HealthIT"
 }
